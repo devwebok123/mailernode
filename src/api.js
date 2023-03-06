b09a1fedc8b5f5d8acdcd182e20835131cb49ea4
devwebok123 <dev.webok123@gmail.com>
2023-03-06 10:44:43 -0600

    update params

--- src/api.js
+++ src/api.js
@@ -24,63 +24,11 @@ router.post("/sendmail", (req, res) => {
     }
   });
 
-  const showIncubator = req.body.data.type == "Founder" ? "" : "none";
-
   var mailOptions = {
     from: req.body.data.email,
-    to: "rare@bmverse.io",
-    subject: "Collaborate",
-    html: `<table>
-      <tbody style={{ fontSize: '20px' }}>
-        <tr>
-          <th>Type</th>
-          <td>${req.body.data.type}</td>
-        </tr>
-        <tr>
-          <th>Name</th>
-          <td>${req.body.data.name}</td>
-        </tr>
-        <tr>
-          <th>Email</th>
-          <td>${req.body.data.email}</td>
-        </tr>
-        <tr>
-          <th>Conversance</th>
-          <td>${req.body.data.conversance}</td>
-        </tr>
-        <tr>
-          <th>Innated talent</th>
-          <td>${req.body.data.talent}</td>
-        </tr>
-        <tr>
-          <th>Demand type</th>
-          <td>${req.body.data.demand}</td>
-        </tr>
-        <tr>
-          <th>Linkedin</th>
-          <td>${req.body.data.linked}</td>
-        </tr>
-        <tr>
-          <th>Twitter</th>
-          <td>${req.body.data.twitter}</td>
-        </tr>
-        <tr>
-          <th>Github</th>
-          <td>${req.body.data.github}</td>
-        </tr>
-        <tr>
-          <th>Discord</th>
-          <td>${req.body.data.discord}</td>
-        </tr>
-        <tr style="display:${showIncubator}">
-          <th>Incubator / Accelerator</th>
-          <td>${req.body.data.incubator}</td>
-        </tr>
-        <tr>
-          <th>Fiction</th>
-          <td>${req.body.data.fiction}</td>
-        </tr>
-      </tbody></table>`
+    to: req.body.data.toAddress,
+    subject: req.body.data.subject,
+    html: req.body.data.content
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
